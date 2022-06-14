380fd584d43e4b89cf5851b01682e58348d709d6
Akash Mehra <akashmehra@akash.mehra>
2022-06-15 05:21:58 +0530

    Updated next/image

--- components/event-detail/event-logistics.jsx
+++ components/event-detail/event-logistics.jsx
@@ -2,6 +2,8 @@ import AddressIcon from '../../icons/address-icon';
 import DateIcon from '../../icons/date-icon';
 import LogisticsItem from './logistics-item';
 import classes from '../../styles/event-logistics.module.css';
+import Image from "next/image";
+
 
 function EventLogistics(props) {
   const { date, address, image, imageAlt } = props;
@@ -16,7 +18,12 @@ function EventLogistics(props) {
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        <Image
+          src={`/${image}`}
+          alt={imageAlt}
+          width={"200px"}
+          height={"200px"}
+        />
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
